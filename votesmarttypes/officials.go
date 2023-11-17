5db052bde67c20edcf8297f07227cbd9c01c3ec3
John Pedrie <john@freespoke.com>
2023-11-17 13:51:19 -0500

    fixes

--- votesmarttypes/officials.go
+++ votesmarttypes/officials.go
@@ -4,39 +4,7 @@ package votesmarttypes
 // See http://api.votesmart.org/docs/Officials.html for usage details.
 type OfficialsGetStatewide struct {
 	CandidateList struct {
-		Candidate []struct {
-			CandidateID          string `json:"candidateId"`
-			FirstName            string `json:"firstName"`
-			NickName             string `json:"nickName"`
-			MiddleName           string `json:"middleName"`
-			PreferredName        string `json:"preferredName"`
-			LastName             string `json:"lastName"`
-			Suffix               string `json:"suffix"`
-			Title                string `json:"title"`
-			BallotName           string `json:"ballotName"`
-			ElectionParties      string `json:"electionParties"`
-			ElectionStatus       string `json:"electionStatus"`
-			ElectionStage        string `json:"electionStage"`
-			ElectionDistrictID   string `json:"electionDistrictId"`
-			ElectionDistrictName string `json:"electionDistrictName"`
-			ElectionOffice       string `json:"electionOffice"`
-			ElectionOfficeID     string `json:"electionOfficeId"`
-			ElectionStateID      string `json:"electionStateId"`
-			ElectionOfficeTypeID string `json:"electionOfficeTypeId"`
-			ElectionYear         string `json:"electionYear"`
-			ElectionSpecial      string `json:"electionSpecial"`
-			ElectionDate         string `json:"electionDate"`
-			OfficeParties        string `json:"officeParties"`
-			OfficeStatus         string `json:"officeStatus"`
-			OfficeDistrictID     string `json:"officeDistrictId"`
-			OfficeDistrictName   string `json:"officeDistrictName"`
-			OfficeStateID        string `json:"officeStateId"`
-			OfficeID             string `json:"officeId"`
-			OfficeName           string `json:"officeName"`
-			OfficeTypeID         string `json:"officeTypeId"`
-			RunningMateID        string `json:"runningMateId"`
-			RunningMateName      string `json:"runningMateName"`
-		} `json:"candidate"`
+		Candidate MaybeList[Candidate] `json:"candidate"`
 	} `json:"candidateList"`
 }
 
@@ -48,39 +16,7 @@ func (OfficialsGetStatewide) Method() string {
 // See http://api.votesmart.org/docs/Officials.html for usage details.
 type OfficialsGetByOfficeState struct {
 	CandidateList struct {
-		Candidate struct {
-			CandidateID          string `json:"candidateId"`
-			FirstName            string `json:"firstName"`
-			NickName             string `json:"nickName"`
-			MiddleName           string `json:"middleName"`
-			PreferredName        string `json:"preferredName"`
-			LastName             string `json:"lastName"`
-			Suffix               string `json:"suffix"`
-			Title                string `json:"title"`
-			BallotName           string `json:"ballotName"`
-			ElectionParties      string `json:"electionParties"`
-			ElectionStatus       string `json:"electionStatus"`
-			ElectionStage        string `json:"electionStage"`
-			ElectionDistrictID   string `json:"electionDistrictId"`
-			ElectionDistrictName string `json:"electionDistrictName"`
-			ElectionOffice       string `json:"electionOffice"`
-			ElectionOfficeID     string `json:"electionOfficeId"`
-			ElectionStateID      string `json:"electionStateId"`
-			ElectionOfficeTypeID string `json:"electionOfficeTypeId"`
-			ElectionYear         string `json:"electionYear"`
-			ElectionSpecial      string `json:"electionSpecial"`
-			ElectionDate         string `json:"electionDate"`
-			OfficeParties        string `json:"officeParties"`
-			OfficeStatus         string `json:"officeStatus"`
-			OfficeDistrictID     string `json:"officeDistrictId"`
-			OfficeDistrictName   string `json:"officeDistrictName"`
-			OfficeStateID        string `json:"officeStateId"`
-			OfficeID             string `json:"officeId"`
-			OfficeName           string `json:"officeName"`
-			OfficeTypeID         string `json:"officeTypeId"`
-			RunningMateID        string `json:"runningMateId"`
-			RunningMateName      string `json:"runningMateName"`
-		} `json:"candidate"`
+		Candidate MaybeList[Candidate] `json:"candidate"`
 	} `json:"candidateList"`
 }
 
@@ -92,39 +28,7 @@ func (OfficialsGetByOfficeState) Method() string {
 // See http://api.votesmart.org/docs/Officials.html for usage details.
 type OfficialsGetByOfficeTypeState struct {
 	CandidateList struct {
-		Candidate []struct {
-			CandidateID          string `json:"candidateId"`
-			FirstName            string `json:"firstName"`
-			NickName             string `json:"nickName"`
-			MiddleName           string `json:"middleName"`
-			PreferredName        string `json:"preferredName"`
-			LastName             string `json:"lastName"`
-			Suffix               string `json:"suffix"`
-			Title                string `json:"title"`
-			BallotName           string `json:"ballotName"`
-			ElectionParties      string `json:"electionParties"`
-			ElectionStatus       string `json:"electionStatus"`
-			ElectionStage        string `json:"electionStage"`
-			ElectionDistrictID   string `json:"electionDistrictId"`
-			ElectionDistrictName string `json:"electionDistrictName"`
-			ElectionOffice       string `json:"electionOffice"`
-			ElectionOfficeID     string `json:"electionOfficeId"`
-			ElectionStateID      string `json:"electionStateId"`
-			ElectionOfficeTypeID string `json:"electionOfficeTypeId"`
-			ElectionYear         string `json:"electionYear"`
-			ElectionSpecial      string `json:"electionSpecial"`
-			ElectionDate         string `json:"electionDate"`
-			OfficeParties        string `json:"officeParties"`
-			OfficeStatus         string `json:"officeStatus"`
-			OfficeDistrictID     string `json:"officeDistrictId"`
-			OfficeDistrictName   string `json:"officeDistrictName"`
-			OfficeStateID        string `json:"officeStateId"`
-			OfficeID             string `json:"officeId"`
-			OfficeName           string `json:"officeName"`
-			OfficeTypeID         string `json:"officeTypeId"`
-			RunningMateID        string `json:"runningMateId"`
-			RunningMateName      string `json:"runningMateName"`
-		} `json:"candidate"`
+		Candidate MaybeList[Candidate] `json:"candidate"`
 	} `json:"candidateList"`
 }
 
@@ -136,39 +40,7 @@ func (OfficialsGetByOfficeTypeState) Method() string {
 // See http://api.votesmart.org/docs/Officials.html for usage details.
 type OfficialsGetByLastname struct {
 	CandidateList struct {
-		Candidate struct {
-			CandidateID          string `json:"candidateId"`
-			FirstName            string `json:"firstName"`
-			NickName             string `json:"nickName"`
-			MiddleName           string `json:"middleName"`
-			PreferredName        string `json:"preferredName"`
-			LastName             string `json:"lastName"`
-			Suffix               string `json:"suffix"`
-			Title                string `json:"title"`
-			BallotName           string `json:"ballotName"`
-			ElectionParties      string `json:"electionParties"`
-			ElectionStatus       string `json:"electionStatus"`
-			ElectionStage        string `json:"electionStage"`
-			ElectionDistrictID   string `json:"electionDistrictId"`
-			ElectionDistrictName string `json:"electionDistrictName"`
-			ElectionOffice       string `json:"electionOffice"`
-			ElectionOfficeID     string `json:"electionOfficeId"`
-			ElectionStateID      string `json:"electionStateId"`
-			ElectionOfficeTypeID string `json:"electionOfficeTypeId"`
-			ElectionYear         string `json:"electionYear"`
-			ElectionSpecial      string `json:"electionSpecial"`
-			ElectionDate         string `json:"electionDate"`
-			OfficeParties        string `json:"officeParties"`
-			OfficeStatus         string `json:"officeStatus"`
-			OfficeDistrictID     string `json:"officeDistrictId"`
-			OfficeDistrictName   string `json:"officeDistrictName"`
-			OfficeStateID        string `json:"officeStateId"`
-			OfficeID             string `json:"officeId"`
-			OfficeName           string `json:"officeName"`
-			OfficeTypeID         string `json:"officeTypeId"`
-			RunningMateID        string `json:"runningMateId"`
-			RunningMateName      string `json:"runningMateName"`
-		} `json:"candidate"`
+		Candidate MaybeList[Candidate] `json:"candidate"`
 	} `json:"candidateList"`
 }
 
@@ -180,39 +52,7 @@ func (OfficialsGetByLastname) Method() string {
 // See http://api.votesmart.org/docs/Officials.html for usage details.
 type OfficialsGetByLevenshtein struct {
 	CandidateList struct {
-		Candidate struct {
-			CandidateID          string `json:"candidateId"`
-			FirstName            string `json:"firstName"`
-			NickName             string `json:"nickName"`
-			MiddleName           string `json:"middleName"`
-			PreferredName        string `json:"preferredName"`
-			LastName             string `json:"lastName"`
-			Suffix               string `json:"suffix"`
-			Title                string `json:"title"`
-			BallotName           string `json:"ballotName"`
-			ElectionParties      string `json:"electionParties"`
-			ElectionStatus       string `json:"electionStatus"`
-			ElectionStage        string `json:"electionStage"`
-			ElectionDistrictID   string `json:"electionDistrictId"`
-			ElectionDistrictName string `json:"electionDistrictName"`
-			ElectionOffice       string `json:"electionOffice"`
-			ElectionOfficeID     string `json:"electionOfficeId"`
-			ElectionStateID      string `json:"electionStateId"`
-			ElectionOfficeTypeID string `json:"electionOfficeTypeId"`
-			ElectionYear         string `json:"electionYear"`
-			ElectionSpecial      string `json:"electionSpecial"`
-			ElectionDate         string `json:"electionDate"`
-			OfficeParties        string `json:"officeParties"`
-			OfficeStatus         string `json:"officeStatus"`
-			OfficeDistrictID     string `json:"officeDistrictId"`
-			OfficeDistrictName   string `json:"officeDistrictName"`
-			OfficeStateID        string `json:"officeStateId"`
-			OfficeID             string `json:"officeId"`
-			OfficeName           string `json:"officeName"`
-			OfficeTypeID         string `json:"officeTypeId"`
-			RunningMateID        string `json:"runningMateId"`
-			RunningMateName      string `json:"runningMateName"`
-		} `json:"candidate"`
+		Candidate MaybeList[Candidate] `json:"candidate"`
 	} `json:"candidateList"`
 }
 
